eaf84aeeb5057dc5635f45a23e4e0ef43479f659
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-09 00:17:13 +0200

    add docs --resource 🧚🏻‍♂️ ..

---
+++ docs/config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+docute.init({
+  debug: true,
+  routerMode: 'history',
+  title: 'Beenvo',
+  repo: '3imed-jaberi/beenvo',
+  'edit-link': 'https://github.com/3imed-jaberi/beenvo/tree/master',
+  twitter: '3imed_jaberi',
+  nav: {
+    default: [
+      {
+        title: 'The best environment variables loader for Node.js',
+        path: '/'
+      }
+    ]
+  },
+  plugins: [docuteEmojify()]
+});
